382c2ec8e25bfecfcf027e424f09c5513d30e584
daria <daria.microsoft@gmail.com>
2017-12-13 03:35:15 +0100

    Task#4. Create a webtest for Contact creation. Test failed: System.ArgumentNullException: text cannot be null, Parametername: text.

---
+++ addressbookWeb/addressbookWeb/ContactCreationTests.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class ContactCreationTests
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            FirefoxOptions options = new FirefoxOptions();
+            options.UseLegacyImplementation = true;
+            options.BrowserExecutableLocation = @"C:\Program Files (x86)\Mozilla Firefox\firefox.exe";
+
+            driver = new FirefoxDriver(options);
+
+            baseURL = "http://localhost/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void ContactCreation()
+        {
+            OpenHomePage();
+            LogIn(new AccountData("admin", "secret"));
+            AddContact(new ContactData("Max","Ivanov"));
+            // ERROR: Caught exception [Error: Dom locators are not implemented yet!]
+            LogOut();
+        }
+
+        private void LogOut()
+        {
+            driver.FindElement(By.LinkText("Logout")).Click();
+        }
+
+        private void AddContact(ContactData contact)
+        {
+            driver.FindElement(By.LinkText("add new")).Click();
+            driver.FindElement(By.Name("firstname")).Clear();
+            driver.FindElement(By.Name("firstname")).SendKeys(contact.Firstname);
+            driver.FindElement(By.Name("lastname")).Clear();
+            driver.FindElement(By.Name("lastname")).SendKeys(contact.Lastname);
+            //driver.FindElement(By.Name("photo")).Clear();
+            //driver.FindElement(By.Name("photo")).SendKeys("C:\\Users\\daria\\Desktop\\screenshot_2.png");
+            //driver.FindElement(By.Name("company")).Clear();
+            //driver.FindElement(By.Name("company")).SendKeys("Google");
+            //driver.FindElement(By.Name("address")).Clear();
+            //driver.FindElement(By.Name("address")).SendKeys("Moskva");
+            //driver.FindElement(By.Name("email")).Clear();
+            //driver.FindElement(By.Name("email")).SendKeys("max.ivanov@example.com");
+            new SelectElement(driver.FindElement(By.Name("bday"))).SelectByText("1");
+            new SelectElement(driver.FindElement(By.Name("bmonth"))).SelectByText("January");
+            driver.FindElement(By.Name("byear")).Clear();
+            driver.FindElement(By.Name("byear")).SendKeys("2000");
+            new SelectElement(driver.FindElement(By.Name("new_group"))).SelectByText("aaa");
+        }
+
+        private void LogIn(AccountData account)
+        {
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(account.Username);
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
+            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
+        }
+
+        private void OpenHomePage()
+        {
+            driver.Navigate().GoToUrl(baseURL + "addressbook/group.php");
+        }
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
---
+++ addressbookWeb/addressbookWeb/ContactData.cs
@@ -0,0 +1,28 @@
+﻿namespace WebAddressbookTests
+{
+    internal class ContactData
+    {
+        private string firstname;
+        private string lastname;
+        private int dob;
+
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Dob { get; set; }
+
+        public ContactData(string firstname, string lastname)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+
+        public ContactData(string firstname, string lastname, int dob)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.dob = dob;
+        }
+
+
+    }
+}
